a070ca8cea51369dda2c629f7b93846c39219c2e
Kosench <ilyakosenko09@gmail.com>
2026-01-12 23:54:48 +1000

    update model

---
+++ inventory/internal/converter/part.go
@@ -0,0 +1,216 @@
+package converter
+
+import (
+	"github.com/Kosench/go-microservices-ecommerce/inventory/internal/model"
+	repoModel "github.com/Kosench/go-microservices-ecommerce/inventory/internal/repository/model"
+	inventoryv1 "github.com/Kosench/go-microservices-ecommerce/shared/pkg/proto/inventory/v1"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+// ConvertRepoPartToModelPart конвертирует Part из repository model в service model
+func ConvertRepoPartToModelPart(repoPart *repoModel.Part) *model.Part {
+	if repoPart == nil {
+		return nil
+	}
+
+	var dimensions *model.Dimensions
+	if repoPart.Dimensions != nil {
+		dimensions = &model.Dimensions{
+			Length: repoPart.Dimensions.Length,
+			Width:  repoPart.Dimensions.Width,
+			Height: repoPart.Dimensions.Height,
+			Weight: repoPart.Dimensions.Weight,
+		}
+	}
+
+	var manufacturer *model.Manufacturer
+	if repoPart.Manufacturer != nil {
+		manufacturer = &model.Manufacturer{
+			Name:    repoPart.Manufacturer.Name,
+			Country: repoPart.Manufacturer.Country,
+			Website: repoPart.Manufacturer.Website,
+		}
+	}
+
+	return &model.Part{
+		UUID:          repoPart.UUID,
+		Name:          repoPart.Name,
+		Description:   repoPart.Description,
+		Price:         repoPart.Price,
+		StockQuantity: repoPart.StockQuantity,
+		Category:      model.Category(repoPart.Category),
+		Dimensions:    dimensions,
+		Manufacturer:  manufacturer,
+		Tags:          repoPart.Tags,
+		Metadata:      convertRepoMetadataToModelMetadata(repoPart.Metadata),
+		CreatedAt:     repoPart.CreatedAt.Time(),
+		UpdatedAt:     repoPart.UpdatedAt.Time(),
+	}
+}
+
+func convertRepoMetadataToModelMetadata(repoMetadata map[string]*repoModel.Value) map[string]*model.Value {
+	if repoMetadata == nil {
+		return nil
+	}
+
+	modelMetadata := make(map[string]*model.Value)
+	for key, repoValue := range repoMetadata {
+		if repoValue != nil {
+			value := convertRepoValueToModelValue(repoValue)
+			modelMetadata[key] = &value
+		}
+	}
+	return modelMetadata
+}
+
+func convertRepoValueToModelValue(repoValue *repoModel.Value) model.Value {
+	if repoValue == nil {
+		return nil
+	}
+
+	switch v := (*repoValue).(type) {
+	case *repoModel.StringValue:
+		return &model.StringValue{StringValue: v.StringValue}
+	case *repoModel.Int64Value:
+		return &model.Int64Value{Int64Value: v.Int64Value}
+	case *repoModel.DoubleValue:
+		return &model.DoubleValue{DoubleValue: v.DoubleValue}
+	case *repoModel.BoolValue:
+		return &model.BoolValue{BoolValue: v.BoolValue}
+	default:
+		return nil
+	}
+}
+
+// ConvertModelPartsFilterToRepoPartsFilter конвертирует PartsFilter из service model в repository model
+func ConvertModelPartsFilterToRepoPartsFilter(modelFilter *model.PartsFilter) *repoModel.PartsFilter {
+	if modelFilter == nil {
+		return nil
+	}
+
+	return &repoModel.PartsFilter{
+		Uuids:                 modelFilter.Uuids,
+		Names:                 modelFilter.Names,
+		Categories:            convertModelCategoriesToRepoCategories(modelFilter.Categories),
+		ManufacturerCountries: modelFilter.ManufacturerCountries,
+		Tags:                  modelFilter.Tags,
+	}
+}
+
+func convertModelCategoriesToRepoCategories(modelCategories []model.Category) []repoModel.Category {
+	if modelCategories == nil {
+		return nil
+	}
+
+	repoCategories := make([]repoModel.Category, len(modelCategories))
+	for i, category := range modelCategories {
+		repoCategories[i] = repoModel.Category(category)
+	}
+	return repoCategories
+}
+
+// ConvertRepoPartsToModelParts конвертирует массив Parts из repository model в service model
+func ConvertRepoPartsToModelParts(repoParts []*repoModel.Part) []*model.Part {
+	if repoParts == nil {
+		return nil
+	}
+
+	modelParts := make([]*model.Part, len(repoParts))
+	for i, repoPart := range repoParts {
+		modelParts[i] = ConvertRepoPartToModelPart(repoPart)
+	}
+	return modelParts
+}
+
+// convertPartToGRPC конвертирует внутреннюю модель Part в gRPC модель
+func ConvertPartToGRPC(part *model.Part) *inventoryv1.Part {
+	grpcPart := &inventoryv1.Part{
+		Uuid:          part.UUID,
+		Name:          part.Name,
+		Description:   part.Description,
+		Price:         part.Price,
+		StockQuantity: part.StockQuantity,
+		Category:      convertCategoryToGRPC(part.Category),
+		Tags:          part.Tags,
+		CreatedAt:     timestamppb.New(part.CreatedAt),
+		UpdatedAt:     timestamppb.New(part.UpdatedAt),
+	}
+
+	if part.Dimensions != nil {
+		grpcPart.Dimensions = &inventoryv1.Dimensions{
+			Length: part.Dimensions.Length,
+			Width:  part.Dimensions.Width,
+			Height: part.Dimensions.Height,
+			Weight: part.Dimensions.Weight,
+		}
+	}
+
+	if part.Manufacturer != nil {
+		grpcPart.Manufacturer = &inventoryv1.Manufacturer{
+			Name:    part.Manufacturer.Name,
+			Country: part.Manufacturer.Country,
+			Website: part.Manufacturer.Website,
+		}
+	}
+
+	return grpcPart
+}
+
+// convertCategoryToGRPC конвертирует внутреннюю категорию в gRPC категорию
+func convertCategoryToGRPC(category model.Category) inventoryv1.Category {
+	switch category {
+	case model.CategoryEngine:
+		return inventoryv1.Category_CATEGORY_ENGINE
+	case model.CategoryFuel:
+		return inventoryv1.Category_CATEGORY_FUEL
+	case model.CategoryPorthole:
+		return inventoryv1.Category_CATEGORY_PORTHOLE
+	case model.CategoryWing:
+		return inventoryv1.Category_CATEGORY_WING
+	default:
+		return inventoryv1.Category_CATEGORY_UNSPECIFIED
+	}
+}
+
+// ConvertFilterFromGRPC конвертирует gRPC фильтр в модель фильтра
+func ConvertFilterFromGRPC(grpcFilter *inventoryv1.PartsFilter) *model.PartsFilter {
+	if grpcFilter == nil {
+		return &model.PartsFilter{}
+	}
+	return &model.PartsFilter{
+		Uuids:                 grpcFilter.GetUuids(),
+		Names:                 grpcFilter.GetNames(),
+		Categories:            convertGRPCCategoriesToModelCategories(grpcFilter.GetCategories()),
+		ManufacturerCountries: grpcFilter.GetManufacturerCountries(),
+		Tags:                  grpcFilter.GetTags(),
+	}
+}
+
+// convertGRPCCategoriesToModelCategories конвертирует gRPC категории в модель категории
+func convertGRPCCategoriesToModelCategories(grpcCategories []inventoryv1.Category) []model.Category {
+	if grpcCategories == nil {
+		return nil
+	}
+
+	modelCategories := make([]model.Category, len(grpcCategories))
+	for i, category := range grpcCategories {
+		modelCategories[i] = convertGRPCCategoryToModelCategory(category)
+	}
+	return modelCategories
+}
+
+// convertGRPCCategoryToModelCategory конвертирует gRPC категорию в модель категории
+func convertGRPCCategoryToModelCategory(grpcCategory inventoryv1.Category) model.Category {
+	switch grpcCategory {
+	case inventoryv1.Category_CATEGORY_ENGINE:
+		return model.CategoryEngine
+	case inventoryv1.Category_CATEGORY_FUEL:
+		return model.CategoryFuel
+	case inventoryv1.Category_CATEGORY_PORTHOLE:
+		return model.CategoryPorthole
+	case inventoryv1.Category_CATEGORY_WING:
+		return model.CategoryWing
+	default:
+		return model.CategoryUnknown
+	}
+}
---
+++ inventory/internal/repository/converter/part.go
@@ -0,0 +1,230 @@
+package converter
+
+import (
+	"time"
+
+	serviceModel "github.com/Kosench/go-microservices-ecommerce/inventory/internal/model"
+	repoModel "github.com/Kosench/go-microservices-ecommerce/inventory/internal/repository/model"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// ConvertServicePartToRepoPart конвертирует Part из service model в repository model
+func ConvertServicePartToRepoPart(servicePart *serviceModel.Part) *repoModel.Part {
+	if servicePart == nil {
+		return nil
+	}
+
+	var dimensions *repoModel.Dimensions
+	if servicePart.Dimensions != nil {
+		dimensions = &repoModel.Dimensions{
+			Length: servicePart.Dimensions.Length,
+			Width:  servicePart.Dimensions.Width,
+			Height: servicePart.Dimensions.Height,
+			Weight: servicePart.Dimensions.Weight,
+		}
+	}
+
+	var manufacturer *repoModel.Manufacturer
+	if servicePart.Manufacturer != nil {
+		manufacturer = &repoModel.Manufacturer{
+			Name:    servicePart.Manufacturer.Name,
+			Country: servicePart.Manufacturer.Country,
+			Website: servicePart.Manufacturer.Website,
+		}
+	}
+
+	now := primitive.NewDateTimeFromTime(time.Now())
+	return &repoModel.Part{
+		UUID:          servicePart.UUID,
+		Name:          servicePart.Name,
+		Description:   servicePart.Description,
+		Price:         servicePart.Price,
+		StockQuantity: servicePart.StockQuantity,
+		Category:      convertServiceCategoryToRepoCategory(servicePart.Category),
+		Dimensions:    dimensions,
+		Manufacturer:  manufacturer,
+		Tags:          servicePart.Tags,
+		Metadata:      convertServiceMetadataToRepoMetadata(servicePart.Metadata),
+		CreatedAt:     convertServiceTimeToPrimitiveDateTime(servicePart.CreatedAt, now),
+		UpdatedAt:     now, // Всегда обновляем время изменения
+	}
+}
+
+// ConvertRepoPartToServicePart конвертирует Part из repository model в service model
+func ConvertRepoPartToServicePart(repoPart *repoModel.Part) *serviceModel.Part {
+	if repoPart == nil {
+		return nil
+	}
+
+	var dimensions *serviceModel.Dimensions
+	if repoPart.Dimensions != nil {
+		dimensions = &serviceModel.Dimensions{
+			Length: repoPart.Dimensions.Length,
+			Width:  repoPart.Dimensions.Width,
+			Height: repoPart.Dimensions.Height,
+			Weight: repoPart.Dimensions.Weight,
+		}
+	}
+
+	var manufacturer *serviceModel.Manufacturer
+	if repoPart.Manufacturer != nil {
+		manufacturer = &serviceModel.Manufacturer{
+			Name:    repoPart.Manufacturer.Name,
+			Country: repoPart.Manufacturer.Country,
+			Website: repoPart.Manufacturer.Website,
+		}
+	}
+
+	return &serviceModel.Part{
+		UUID:          repoPart.UUID,
+		Name:          repoPart.Name,
+		Description:   repoPart.Description,
+		Price:         repoPart.Price,
+		StockQuantity: repoPart.StockQuantity,
+		Category:      convertRepoCategoryToServiceCategory(repoPart.Category),
+		Dimensions:    dimensions,
+		Manufacturer:  manufacturer,
+		Tags:          repoPart.Tags,
+		Metadata:      convertRepoMetadataToServiceMetadata(repoPart.Metadata),
+		CreatedAt:     convertPrimitiveDateTimeToServiceTime(repoPart.CreatedAt),
+		UpdatedAt:     convertPrimitiveDateTimeToServiceTime(repoPart.UpdatedAt),
+	}
+}
+
+// ConvertServiceFilterToRepoFilter конвертирует PartsFilter из service model в repository model
+func ConvertServiceFilterToRepoFilter(serviceFilter *serviceModel.PartsFilter) *repoModel.PartsFilter {
+	if serviceFilter == nil {
+		return nil
+	}
+
+	var categories []repoModel.Category
+	if serviceFilter.Categories != nil {
+		categories = make([]repoModel.Category, len(serviceFilter.Categories))
+		for i, category := range serviceFilter.Categories {
+			categories[i] = convertServiceCategoryToRepoCategory(category)
+		}
+	}
+
+	return &repoModel.PartsFilter{
+		Uuids:                 serviceFilter.Uuids,
+		Names:                 serviceFilter.Names,
+		Categories:            categories,
+		ManufacturerCountries: serviceFilter.ManufacturerCountries,
+		Tags:                  serviceFilter.Tags,
+	}
+}
+
+// Конвертеры для категорий
+func convertServiceCategoryToRepoCategory(serviceCategory serviceModel.Category) repoModel.Category {
+	switch serviceCategory {
+	case serviceModel.CategoryEngine:
+		return repoModel.CategoryEngine
+	case serviceModel.CategoryFuel:
+		return repoModel.CategoryFuel
+	case serviceModel.CategoryPorthole:
+		return repoModel.CategoryPorthole
+	case serviceModel.CategoryWing:
+		return repoModel.CategoryWing
+	default:
+		return repoModel.CategoryUnknown
+	}
+}
+
+func convertRepoCategoryToServiceCategory(repoCategory repoModel.Category) serviceModel.Category {
+	switch repoCategory {
+	case repoModel.CategoryEngine:
+		return serviceModel.CategoryEngine
+	case repoModel.CategoryFuel:
+		return serviceModel.CategoryFuel
+	case repoModel.CategoryPorthole:
+		return serviceModel.CategoryPorthole
+	case repoModel.CategoryWing:
+		return serviceModel.CategoryWing
+	default:
+		return serviceModel.CategoryUnknown
+	}
+}
+
+// Конвертеры для метаданных
+func convertServiceMetadataToRepoMetadata(serviceMetadata map[string]*serviceModel.Value) map[string]*repoModel.Value {
+	if serviceMetadata == nil {
+		return nil
+	}
+
+	repoMetadata := make(map[string]*repoModel.Value)
+	for key, serviceValue := range serviceMetadata {
+		if serviceValue != nil {
+			repoMetadata[key] = convertServiceValueToRepoValue(serviceValue)
+		}
+	}
+	return repoMetadata
+}
+
+func convertRepoMetadataToServiceMetadata(repoMetadata map[string]*repoModel.Value) map[string]*serviceModel.Value {
+	if repoMetadata == nil {
+		return nil
+	}
+
+	serviceMetadata := make(map[string]*serviceModel.Value)
+	for key, repoValue := range repoMetadata {
+		if repoValue != nil {
+			serviceMetadata[key] = convertRepoValueToServiceValue(repoValue)
+		}
+	}
+	return serviceMetadata
+}
+
+// Конвертеры для Value типов
+func convertServiceValueToRepoValue(serviceValue *serviceModel.Value) *repoModel.Value {
+	if serviceValue == nil {
+		return nil
+	}
+
+	var value repoModel.Value
+	switch v := (*serviceValue).(type) {
+	case *serviceModel.StringValue:
+		value = &repoModel.StringValue{StringValue: v.StringValue}
+	case *serviceModel.Int64Value:
+		value = &repoModel.Int64Value{Int64Value: v.Int64Value}
+	case *serviceModel.DoubleValue:
+		value = &repoModel.DoubleValue{DoubleValue: v.DoubleValue}
+	case *serviceModel.BoolValue:
+		value = &repoModel.BoolValue{BoolValue: v.BoolValue}
+	default:
+		return nil
+	}
+	return &value
+}
+
+func convertRepoValueToServiceValue(repoValue *repoModel.Value) *serviceModel.Value {
+	if repoValue == nil {
+		return nil
+	}
+
+	var value serviceModel.Value
+	switch v := (*repoValue).(type) {
+	case *repoModel.StringValue:
+		value = &serviceModel.StringValue{StringValue: v.StringValue}
+	case *repoModel.Int64Value:
+		value = &serviceModel.Int64Value{Int64Value: v.Int64Value}
+	case *repoModel.DoubleValue:
+		value = &serviceModel.DoubleValue{DoubleValue: v.DoubleValue}
+	case *repoModel.BoolValue:
+		value = &serviceModel.BoolValue{BoolValue: v.BoolValue}
+	default:
+		return nil
+	}
+	return &value
+}
+
+// Конвертеры для времени
+func convertServiceTimeToPrimitiveDateTime(serviceTime time.Time, fallback primitive.DateTime) primitive.DateTime {
+	if serviceTime.IsZero() {
+		return fallback
+	}
+	return primitive.NewDateTimeFromTime(serviceTime)
+}
+
+func convertPrimitiveDateTimeToServiceTime(repoTime primitive.DateTime) time.Time {
+	return repoTime.Time()
+}
