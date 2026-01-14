f5df612c64ce9af3fd8c60ce5f7588594c3ccbbe
Kosench <ilyakosenko09@gmail.com>
2026-01-14 22:28:54 +1000

    fix

--- inventory/internal/service/interface.go
+++ inventory/internal/service/interface.go
@@ -4,12 +4,9 @@ import (
 	"context"
 
 	"github.com/Kosench/go-microservices-ecommerce/inventory/internal/model"
-	inventoryv1 "github.com/Kosench/go-microservices-ecommerce/shared/pkg/proto/inventory/v1"
 )
 
 type Service interface {
 	GetPart(ctx context.Context, uuid string) (*model.Part, error)
 	ListParts(ctx context.Context, filter *model.PartsFilter) ([]*model.Part, error)
-
-	inventoryv1.UnimplementedInventoryServiceServer
 }
